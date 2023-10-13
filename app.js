ffd090cab8041e2fd332473452f38b7f4ff80ef3
rahmanaripon <rjripon268@gmail.com>
2023-10-13 22:31:28 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let ProthomNumber = prompt("enter prothom number");
+console.log(ProthomNumber);
+let ditioNumber = prompt("ditio number dao");
+console.log(ditioNumber);
+let sum = ProthomNumber + ditioNumber;
+console.log(sum);
